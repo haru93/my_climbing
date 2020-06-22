ea790f8250f06e545f05f048b724728b3d995fd1
haru93 <syuesuta@yahoo.co.jp>
2020-06-22 13:26:13 +0900

    4 commit

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -4,16 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Follower extends Model
+class Favorite extends Model
 {
-    protected $primaryKey = [
-        'following_id',
-        'followed_id'
-    ];
-    protected $fillable = [
-        'following_id',
-        'followed_id'
-    ];
     public $timestamps = false;
-    public $incrementing = false;
 }
